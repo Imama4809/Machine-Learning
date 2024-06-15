9c077d14dee074e1c4f4a72736b987270617e472
Imama4809 <89104562+Imama4809@users.noreply.github.com>
2024-06-14 21:42:52 -0400

    Update Basic_MNIST_using_CNN.py

--- Basic_MNIST_using_CNN.py
+++ Basic_MNIST_using_CNN.py
@@ -20,9 +20,9 @@ test_loader = DataLoader(test_data,batch_size = 64, shuffle = True)
 class CNN(nn.Module):
     def __init__(self):
         super(CNN,self).__init__()
-        self.layer1 = nn.Sequential(nn.Conv2d(1,32,kernel_size = 3,stride = 1,padding =1),nn.ReLU(),nn.MaxPool2d(kernel_size=2,stride=2))
-        self.layer2 = nn.Sequential(nn.Conv2d(32,64,kernel_size=3),nn.ReLU(),nn.MaxPool2d(2))
-        self.fc1 = nn.Linear(64*6*6,1000)
+        self.layer1 = nn.Sequential(nn.Conv2d(1,500,kernel_size = 3,stride = 1,padding =1),nn.ReLU(),nn.MaxPool2d(kernel_size=2,stride=2))
+        self.layer2 = nn.Sequential(nn.Conv2d(500,1000,kernel_size=3),nn.ReLU(),nn.MaxPool2d(2))
+        self.fc1 = nn.Linear(1000*6*6,1000)
         self.fc2 = nn.Linear(1000,10)
     def forward(self,x):
         x = self.layer1(x)
